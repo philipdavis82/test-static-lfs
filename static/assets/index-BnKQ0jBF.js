e26fe35dbce166fa0ef5885497b41ea254ea6ec4
Philip Davis <philip.davis.me@gmail.com>
2025-08-17 15:21:12 -0500

    testing staic assets

--- static/assets/index-BnKQ0jBF.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:03960e6931670422a0252d8d62cb03f3cbc8028a669f8563b481eb54b59b4afe
-size 188078
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: 'static',
+  build: {
+    outDir: 'static',
+  },
   base: '/test_static_lfs',
 })
