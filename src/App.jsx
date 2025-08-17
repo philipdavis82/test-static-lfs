03900bd6922d98d04969f1b0b7522f5087663287
Philip Davis <philip.davis.me@gmail.com>
2025-08-17 17:25:33 -0500

    Added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,32 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import pieImg from './assets/pie.jpg'
 import './App.css'
+import { Button } from "@/components/ui/button"
+import NoPage from "./pages/NoPage";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+// import.meta.env.BASE_URL;
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div>
-        <img src={pieImg} className="pi" alt="Static Logo" />
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"+import.meta.env.BASE_URL}  element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
